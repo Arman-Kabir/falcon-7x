41249796a5abf6006b0811d35909a5ee898c5858
Arman-Kabir <armankabir8@gmail.com>
2022-04-04 17:01:40 +0600

    home and reviews page responsive done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,8 +12,8 @@ const Home = () => {
     const reviewsThree = reviews.slice(0, 3);
     return (
         <div className='home-container'>
-            <div className='hero-section grid grid-cols-2 my-20 w-10/12 mx-auto'>
-                <div className='hero-details flex flex-col justify-center items-start'>
+            <div className='hero-section grid grid-cols-1 my-5 md:grid-cols-2  md:my-20 w-10/12 mx-auto'>
+                <div className='hero-details text-center mb-5 md:flex flex-col justify-center items-start'>
                     <h3 className='text-4xl text-lime-600 font-semibold'>Buy a Private jet</h3>
                     <h1 className='text-6xl text-red-600 font-bold'>Falcon-7x</h1>
                     <p className='text-slate-600 text-left'> Having a private jet is a symbol of being super rich. If u want to use a private jet for your family use or business meeting or any other work, this is the best plane u will ever have in the sky.</p>
@@ -26,7 +26,7 @@ const Home = () => {
             {/* Reviews Section */}
             <div>
                 <h1 className='text-6xl'>Customer Reviews({reviews.length})</h1>
-                <div className='grid grid-cols-3 gap-4 w-3/4 mx-auto mt-10'>
+                <div className='grid md:grid-cols-2 xl:grid-cols-3 gap-4 w-3/4 mx-auto mt-10'>
                     {
                         reviewsThree.map(review =>
                             <Review
