655e1464b154124c25a68e2866061b778d8c720e
Arman-Kabir <armankabir8@gmail.com>
2022-04-04 16:38:45 +0600

    chart added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Area, AreaChart, CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+
 
 const Dashboard = () => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setData(data))
+    }, []);
+    console.log(data);
+
     return (
         <div>
-            <h1>Dashboard Page</h1>
+            <h1 className='text-4xl font-bold text-blue-400 my-10'>It's The Overall Overview of our product</h1>
+
+            <div className='grid grid-cols-2 gap-6'>
+                {/* Line Chart */}
+                <div className='line-chart'>
+                    <h2 className='text-xl text-blue-400'>Month Wise Sell</h2>
+                    <LineChart width={400} height={300} data={data}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey={'month'}></XAxis>
+                        <YAxis dataKey={'sell'}></YAxis>
+                        <Line type={'monotone'} dataKey={'sell'} stroke="#8884d8"></Line>
+                        <Tooltip></Tooltip>
+                        <Legend></Legend>
+                    </LineChart>
+                </div>
+                {/*Area Chart  */}
+                <div className='area-chart'>
+                    <h2 className='text-xl text-blue-400'>Investment vs Revenue</h2>
+                    <AreaChart width={400} height={300} data={data}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey={'month'}></XAxis>
+                        <YAxis dataKey={'investment'}></YAxis>
+                        {/* <YAxis dataKey={'revenue'}></YAxis> */}
+                        <Area type={'monotone'} dataKey={'investment'} stroke={'green'} fill={'green'}></Area>
+                        <Area type={'monotone'} dataKey={'revenue'} stroke="purple" fill={'purple'}></Area>
+                        <Tooltip></Tooltip>
+                        <Legend></Legend>
+                    </AreaChart>
+                </div>
+
+            </div>
+
+
+
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,11 +12,11 @@ const Home = () => {
     const reviewsThree = reviews.slice(0, 3);
     return (
         <div className='home-container'>
-            <div className='hero-section grid grid-cols-2 py-20 px-40'>
-                <div className='px-40 hero-details flex flex-col justify-center items-start'>
-                    <h3 className='text-4xl text-lime-600 py-5 font-semibold'>Buy a Private jet</h3>
+            <div className='hero-section grid grid-cols-2 my-20 w-10/12 mx-auto'>
+                <div className='hero-details flex flex-col justify-center items-start'>
+                    <h3 className='text-4xl text-lime-600 font-semibold'>Buy a Private jet</h3>
                     <h1 className='text-6xl text-red-600 font-bold'>Falcon-7x</h1>
-                    <p className='py-7 text-slate-600 text-left'> Having a private jet is a symbol of being super rich. If u want to use a private jet for your family use or business meeting or any other work, this is the best plane u will ever have in the sky.</p>
+                    <p className='text-slate-600 text-left'> Having a private jet is a symbol of being super rich. If u want to use a private jet for your family use or business meeting or any other work, this is the best plane u will ever have in the sky.</p>
                 </div>
                 <div className='hero-image flex justify-center'>
                     <img className='w-3/4 rounded-3xl' src={privateJet} alt="" />
